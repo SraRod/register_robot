331314c20779553cfb5b9c4fc7cee8d32dac6a51
Sra Lin <cwsralin@gmail.com>
2023-05-30 22:55:42 +0800

    add time precision and add wait mode

--- multiple_register.sh
+++ multiple_register.sh
@@ -1,3 +1,4 @@
-python main.py 05:58:45 06:00:00 &
-python main.py 05:59:00 06:00:03 &
+python main.py 05:58:45 05:59:59 --delay 0.9 &
+python wait_from_website_time.py 05:59:00 06:00:00 &
+python wait_from_website_time.py 05:59:00 06:00:00 --delay -0.5 &
 wait
---
+++ timesync.py
@@ -0,0 +1,30 @@
+import pytz
+import requests
+import datetime
+import subprocess
+
+def sync_pc_time(ntp_server = 'time.apple.com'):
+    try:
+        subprocess.run(["w32tm", "/config", "/syncfromflags:manual", f"/manualpeerlist:{ntp_server}", "/update"])
+        subprocess.run(["w32tm", "/resync", "/nowait"])
+        print("System time synchronized successfully.")
+    except Exception as e:
+        print(f"Error synchronizing system time: {e}")
+
+def get_server_time(url):
+    try:
+        response = requests.head(url, verify=False)
+        server_time = response.headers['Date']
+        datetime_object = datetime.datetime.strptime(server_time, '%a, %d %b %Y %H:%M:%S %Z')
+        datetime_object = pytz.timezone('GMT').localize(datetime_object).astimezone(pytz.timezone('Asia/Taipei'))
+        return datetime_object
+    except Exception as e:
+        print(f"Error retrieving server time: {e}")
+        return None
+
+def calculate_time_difference(server_time, target_time):
+    target_time = datetime.datetime.strptime(target_time, '%H:%M:%S').time()
+    target_time = datetime.datetime.combine(datetime.datetime.now().date(), target_time).astimezone(pytz.timezone('Asia/Taipei'))
+    time_difference = target_time - server_time
+    seconds_difference = time_difference.total_seconds()
+    return seconds_difference
